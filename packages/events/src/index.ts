2555b7f70583456784cac2ed954d63245f560731
Danial Manavi <dmanavi@live.com>
2024-06-20 23:39:12 -0700

    chore: packages dependencies

--- packages/events/src/index.ts
+++ packages/events/src/index.ts
@@ -1,1 +1,11 @@
-export * from './lib/events.module';
+// errors
+export * from '#shared/errors/event-publishing-failed';
+export * from '#shared/errors/event-subscription-failed';
+
+// models
+export * from '#shared/models/application-event';
+
+// services
+export * from '#shared/services/event-publisher';
+export * from '#shared/services/event-subscriber';
+export * from '#shared/services/event-subscription';
